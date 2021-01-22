db3ff83bc7035b0eeecc3fe87ac02cf7a4637d6e
Elliot Plant <elliotaplant@gmail.com>
2021-01-22 14:47:48 -0800

    Get mailBook working

--- functions/mailBook.js
+++ functions/mailBook.js
@@ -1,7 +1,26 @@
 const mail = require('../utils/mail');
 const respondWith = require('../utils/respondWith');
 
-exports.handler = async function () {
-  await mail('elliotsstorage@gmail.com', 'licence.txt', 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE');
+exports.handler = async function (event) {
+  if (!event.body) {
+    console.error('Missing request body');
+    return respondWith(400, { error: 'Missing request body' });
+  }
+
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    console.error('Invalid non-JSON body: ' + event.body);
+    return respondWith(400, { error: 'Invalid non-JSON body: ' + event.body });
+  }
+
+  const { email, filename, url } = body;
+  if (!(email && filename && url)) {
+    console.error('Missing required fields [email, filename, url]');
+    return respondWith(400, { error: 'Missing required fields [email, filename, url]' });
+  }
+
+  await mail(email, filename, url);
   return respondWith(200, { hello: 'world' });
 };
---
+++ functions/query.js
@@ -0,0 +1,4 @@
+exports.handler = function() {
+  // Send the query to libgen
+  // return libgens response
+};
