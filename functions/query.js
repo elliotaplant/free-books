d8e293f083437d7e17c3b0c1ef7e141286424fe4
Elliot Plant <elliotaplant@gmail.com>
2021-01-24 00:29:56 -0800

    Check for best mirror every time

--- functions/query.js
+++ functions/query.js
@@ -1,18 +1,16 @@
 const libgen = require('libgen');
 const validateRequest = require('../utils/validateRequest');
 const respondWith = require('../utils/respondWith');
-const MIRROR = 'http://libgen.is';
 
 exports.handler = async function(event) {
+  const mirror = await libgen.mirror();
+
   const validationError = validateRequest(event, 'query');
   if (validationError) {
     return validationError;
   }
 
-  const options = {
-    mirror: MIRROR,
-    query: JSON.parse(event.body).query,
-  };
+  const options = { mirror, query: JSON.parse(event.body).query };
   const data = await libgen.search(options);
   return respondWith(200, data);
 };
