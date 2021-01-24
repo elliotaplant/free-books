db3b3fa3ef326d968748dc5f0cc147a42ec7448d
Elliot Plant <elliotaplant@gmail.com>
2021-01-24 00:36:13 -0800

    Add logs to query

--- functions/query.js
+++ functions/query.js
@@ -4,13 +4,16 @@ const respondWith = require('../utils/respondWith');
 
 exports.handler = async function(event) {
   const mirror = await libgen.mirror();
+  console.log('Using mirror', mirror);
 
   const validationError = validateRequest(event, 'query');
   if (validationError) {
     return validationError;
   }
 
-  const options = { mirror, query: JSON.parse(event.body).query };
-  const data = await libgen.search(options);
+  const { query } = JSON.parse(event.body);
+  console.log(`Querying for "${query}"`);
+  const data = await libgen.search({ mirror, query });
+  console.log(`Found ${data.length || 0} result ${data.length === 1 ? '' : 's'}`);
   return respondWith(200, data);
 };
