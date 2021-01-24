2a2bfd3e9708021c6d7976b85a89930979eee1a5
Elliot Plant <elliotaplant@gmail.com>
2021-01-23 17:42:02 -0800

    Delete unused files

--- functions/thingey.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function(event, _, callback) {
-  console.log('THINGKEY');
-  console.log('event', event);
-  console.log('Calling callback with 200');
-  callback(null, { statusCode: 200 });
-  // could it be that this returns a promise no matter what? so it ignores the callback??
-  await new Promise(r => setTimeout(r, 10000));
-  console.log('Finished event body thing');
-};
