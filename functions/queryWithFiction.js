a455318f3994727968e26596c23da0b36f5ff603
Elliot Plant <elliotaplant@gmail.com>
2021-12-30 22:21:33 -0800

    Log errors in queryWithFiction

--- functions/queryWithFiction.js
+++ functions/queryWithFiction.js
@@ -3,16 +3,21 @@ const respondWith = require("../utils/respondWith");
 const queryLibgen = require("../utils/queryLibgen");
 
 exports.handler = async function (event) {
-  const validationError = validateRequest(event, "query");
-  if (validationError) {
-    return validationError;
-  }
+  try {
+    const validationError = validateRequest(event, "query");
+    if (validationError) {
+      return validationError;
+    }
 
-  const { query } = JSON.parse(event.body);
-  console.log(`Querying for "${query}"`);
-  const data = await queryLibgen(query);
-  console.log(
-    `Found ${data.length || 0} result${data.length === 1 ? "" : "s"}`
-  );
-  return respondWith(200, data);
+    const { query } = JSON.parse(event.body);
+    console.log(`Querying for "${query}"`);
+    const data = await queryLibgen(query);
+    console.log(
+      `Found ${data.length || 0} result${data.length === 1 ? "" : "s"}`
+    );
+    return respondWith(200, data);
+  } catch (e) {
+    console.log('error', e);
+    return respondWith(500, { error: e.toString() })
+  }
 };
