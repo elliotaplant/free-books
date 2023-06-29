ab95ce29fb79389a9671e22bac7583ad04d962d6
Elliot Plant <elliotaplant@gmail.com>
2023-06-28 21:40:44 -0700

    Clean it all up

--- server/clen.js
+++
@@ -1,3 +0,0 @@
-const queryLibgen = require('../utils/queryLibgen');
-
-queryLibgen('royal assassin').then(console.log).catch(console.error);
--- utils/fileStuff.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  getFilename: (url) => decodeURIComponent(url.split('/').slice(-1)[0]),
-  getExtension: (url) => url.split('.').slice(-1)[0],
-  changeExtension: (filename, newExtension) =>
-    [...filename.split('.').slice(0, -1), newExtension].join('.'),
-};
