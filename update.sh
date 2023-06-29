973259cb37fc1c4d4392c2907661805a0f7922de
Elliot Plant <elliotaplant@gmail.com>
2023-06-29 10:25:34 -0700

    Some "improvements"

--- update.sh
+++ update.sh
@@ -2,46 +2,25 @@
 
 set -e
 
-# Dependencies: curl, unrar, mysql, pscale
+# Helper function to check if a command is available
+check_command() {
+    if command -v $1 >/dev/null 2>&1; then
+        echo "$1 is installed, proceeding..."
+    else
+        echo "$1 could not be found. Please install it."
+        exit 1
+    fi
+}
 
-# Check if curl, unrar, mysql, and pscale are installed
-if ! command -v curl &> /dev/null
-then
-    echo "curl could not be found. Please install curl."
-    exit 1
-fi
+# Check dependencies
+dependencies=("curl" "unrar" "mysql" "pscale")
+for cmd in "${dependencies[@]}"; do
+    check_command $cmd
+done
 
-if ! command -v unrar &> /dev/null
-then
-    echo "unrar could not be found. Please install unrar."
-    exit 1
-fi
-
-if ! command -v mysql &> /dev/null
-then
-    echo "mysql could not be found. Please install mysql."
-    exit 1
-fi
-
-if ! command -v pscale &> /dev/null
-then
-    echo "pscale could not be found. Please install pscale."
-    exit 1
-fi
-
-# Get MySQL user and password from environment
-MYSQL_USER=${MYSQL_USER}
-MYSQL_PASS=${MYSQL_PASS}
-
-if [ -z "$MYSQL_USER" ]
-then
-    echo "Error: MySQL user not set in environment"
-    exit 1
-fi
-
-if [ -z "$MYSQL_PASS" ]
-then
-    echo "Error: MySQL password not set in environment"
+# Check if .my.cnf file exists
+if [ ! -f ~/.my.cnf ]; then
+    echo "Error: .my.cnf file does not exist in the home directory"
     exit 1
 fi
 
@@ -50,9 +29,10 @@ mkdir -p working
 cd working
 
 # Download dbdumps files
-echo "Downloading dbdumps files..."
-curl -LO https://data.library.bz/dbdumps/fiction.rar 
-curl -LO https://data.library.bz/dbdumps/libgen_compact.rar
+# CHECK IF FILES EXIST??
+# echo "Downloading dbdumps files..."
+# curl -vLO --retry 999 --retry-max-time 0 -C - https://data.library.bz/dbdumps/fiction.rar 
+# curl -vLO --retry 999 --retry-max-time 0 -C - https://data.library.bz/dbdumps/libgen_compact.rar
 
 # Check if both files exist or exit with an error
 if [ ! -f "fiction.rar" ] || [ ! -f "libgen_compact.rar" ]; then
@@ -60,9 +40,10 @@ if [ ! -f "fiction.rar" ] || [ ! -f "libgen_compact.rar" ]; then
     exit 1
 fi
 
-# Extract the downloaded files
-unrar x fiction.rar
-unrar x libgen_compact.rar
+# # Extract the downloaded files
+# # CHECK IF FILES EXIST??
+# unrar x fiction.rar
+# unrar x libgen_compact.rar
 
 # Check if SQL files exist or exit with an error
 if [ ! -f "fiction.sql" ] || [ ! -f "libgen_compact.sql" ]; then
@@ -71,34 +52,38 @@ if [ ! -f "fiction.sql" ] || [ ! -f "libgen_compact.sql" ]; then
 fi
 
 # Start MySQL server
-mysql.server start
+if ! mysqladmin ping -h localhost --silent; then
+    mysql.server start
+else
+    echo "MySQL server is already running"
+fi
 
 # Drop the existing database and create a new one
-mysql -u $MYSQL_USER -p $MYSQL_PASS -e "DROP DATABASE IF EXISTS libgen_combined; CREATE DATABASE libgen_combined;"
+mysql -e "DROP DATABASE IF EXISTS libgen_combined; CREATE DATABASE libgen_combined;"
 
 # Import the SQL files into the database
-mysql -u $MYSQL_USER -p $MYSQL_PASS libgen_combined < fiction.sql
-mysql -u $MYSQL_USER -p $MYSQL_PASS libgen_combined < libgen_compact.sql
+mysql libgen_combined < fiction.sql
+mysql libgen_combined < libgen_compact.sql
 
 # Check if tables 'fiction' and 'updated' exist in the database
-TABLES_EXIST=$(mysql -u $MYSQL_USER -p $MYSQL_PASS -Nse "USE libgen_combined; SHOW TABLES LIKE 'fiction'; SHOW TABLES LIKE 'updated';" | wc -l)
+TABLES_EXIST=$(mysql -Nse "USE libgen_combined; SHOW TABLES LIKE 'fiction'; SHOW TABLES LIKE 'updated';" | wc -l)
 if [ "$TABLES_EXIST" -ne 2 ]; then
     echo "Error: Tables 'fiction' and 'updated' do not exist in the database"
     exit 1
 fi
 
 # Create combined table and run queries
-mysql -u $MYSQL_USER -p $MYSQL_PASS libgen_combined <<SQL
+mysql libgen_combined <<SQL
 CREATE TABLE combined (MD5 CHAR(32), Title TEXT, Author TEXT, Filesize INT);
 INSERT INTO combined SELECT MD5, Title, Author, Filesize FROM fiction WHERE Language = 'English' AND (Extension = 'epub' OR Extension = 'mobi') AND Filesize <= 25000000;
 INSERT INTO combined SELECT MD5, Title, Author, Filesize FROM updated WHERE Language = 'English' AND (Extension = 'epub' OR Extension = 'mobi') AND Filesize <= 25000000;
 SQL
 
 # Print the number of rows in the combined table
-mysql -u $MYSQL_USER -p $MYSQL_PASS libgen_combined -e "SELECT COUNT(*) FROM combined;"
+mysql libgen_combined -e "SELECT COUNT(*) FROM combined;"
 
 # Export the combined table to an SQL file
-mysqldump -u $MYSQL_USER -p $MYSQL_PASS libgen_combined combined > combined.sql
+mysqldump libgen_combined combined > combined.sql
 
 # Use pscale to upload combined table to planetscale
 pscale shell libgen-english main < combined.sql
